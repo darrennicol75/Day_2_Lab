04933a7359e7bfc09b8cf6a84ca2d8490007e2ec
@darrennicol75 <darren.nicol75@gmail.com>
2019-07-30 11:25:58 +0100

    Initial Commit

---
+++ src/test/java/com/codeclan/example/employeeservice/EmployeeserviceApplicationTests.java
@@ -0,0 +1,36 @@
+package com.codeclan.example.employeeservice;
+
+import com.codeclan.example.employeeservice.models.Department;
+import com.codeclan.example.employeeservice.models.Employee;
+import com.codeclan.example.employeeservice.repositories.DepartmentRepository;
+import com.codeclan.example.employeeservice.repositories.EmployeeRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeserviceApplicationTests {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Autowired
+	DepartmentRepository departmentRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canSaveEmployee(){
+		Department marketingDept = new Department("Marketing");
+		departmentRepository.save(marketingDept);
+
+		Employee jim = new Employee("Jim", "McDonald", "123456", marketingDept);
+		employeeRepository.save(jim);
+	}
+
+}
